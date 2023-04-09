487286b0b7e142c829acf88e80eda40021f01a9f
Davlat Uralov <davlat.uralov@gmail.com>
2023-04-09 16:07:47 -0700

    added POST submission from html form

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express")
 const path = require('path')
+var bodyParser = require('body-parser')
 
 const app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/scss', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/scss')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.use(express.static('public'))
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, './public/views/index.html'))
 });
@@ -15,6 +23,10 @@ app.get("/", (req, res) => {
 app.get("/add_contact", (req, res) => {
   res.sendFile(path.join(__dirname, './public/views/add_contact_form.html'))
 });
+
+app.post("/add_contact_request", urlencodedParser, (req,res) =>{
+  res.sendStatus(200);
+})
 
 app.listen(8080, () => {
   console.log('Listening on port ' + 8080);
