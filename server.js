96569039376d86837eb48300f86efa7d987dd0cf
Davlat Uralov <davlat.uralov@gmail.com>
2023-04-09 11:41:31 -0700

    added scss, compile with scss style.scss style.css

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const path = require('path')
 const app = express();
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/scss', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/scss')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-app.use(express.static('images'))
+app.use(express.static('public'))
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, './views/index.html'))
+  res.sendFile(path.join(__dirname, './public/views/index.html'))
 });
 
 app.get("/add_contact", (req, res) => {
-  res.sendFile(path.join(__dirname, './views/add_contact_form.html'))
+  res.sendFile(path.join(__dirname, './public/views/add_contact_form.html'))
 });
 
 app.listen(8080, () => {
